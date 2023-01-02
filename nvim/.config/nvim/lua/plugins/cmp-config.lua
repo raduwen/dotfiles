3a9235bc6576a4dce4a2f9f63fc302137897f071
raduwen <raduwen@gmail.com>
2023-01-02 15:45:48 +0900

    tab/s-tab completion

--- nvim/.config/nvim/lua/plugins/cmp-config.lua
+++ nvim/.config/nvim/lua/plugins/cmp-config.lua
@@ -37,6 +37,11 @@ if vim.fn.executable("mocword") then
   table.insert(cmp_sources, { name = "mocword" })
 end
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
   sources = cmp_sources,
   mapping = {
@@ -46,6 +51,22 @@ cmp.setup({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
   formatting = {
     format = lspkind.cmp_format({
